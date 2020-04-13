aa031cc90e7e1eb415a4ea9ace32a923e4df2eaa
yunchao.zheng <yunchao.zheng@dbappsecurity.com.cn>
2020-04-13 14:57:18 +0800

    add consumer

---
+++ service-consumer/src/main/java/com/chaozai/service/consumer/ServiceConsumerApplication.java
@@ -0,0 +1,47 @@
+package com.chaozai.service.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConsumerApplication.class, args);
+	}
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	// eureka客户端，可以获取到eureka中服务的信息
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@RequestMapping("/hello")
+	public String hello(String name) {
+
+		// 根据服务名称，获取服务实例。有可能是集群，所以是service实例集合
+		List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+		// 因为只有一个service-provider 所以获取第一个实例
+		ServiceInstance instance = instances.get(0);
+		// 获取ip和端口信息，拼接成服务地址
+		String baseUrl = "http://" + instance.getHost() + ":" + instance.getPort() + "/hello?name="+name;
+		return restTemplate.getForObject(baseUrl, String.class);
+	}
+}
